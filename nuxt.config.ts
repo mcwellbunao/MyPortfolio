13b04f2ad0c5ffabacd821b414823f93b5b94688
mcwellbunao <67498562+mcwellbunao@users.noreply.github.com>
2025-06-11 12:46:31 +0800

    061120251246
    testing deployment settings for gh pages test 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
-  css: [
-    '@/assets/styles.css'
-  ],
-  modules: [
-    '@nuxtjs/tailwindcss',
-  ],
-  ssr: true,
-  router: {
-    options: {
-      hashMode: false
-    }
+  css: ['@/assets/styles.css'],
+  modules: ['@nuxtjs/tailwindcss'],
+
+  // ❗️Change this:
+  ssr: false, // disable server-side rendering
+
+  // ✅ Add this:
+  nitro: {
+    preset: 'static'
   },
+
   app: {
     baseURL: '/MyPortfolio/',
   }
