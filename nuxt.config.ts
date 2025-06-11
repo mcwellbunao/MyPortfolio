2a10fc864cf8a8d98be4c9939c60c8431c9eea74
mcwellbunao <67498562+mcwellbunao@users.noreply.github.com>
2025-06-11 12:52:13 +0800

    061120251252
    returned to default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
-  css: ['@/assets/styles.css'],
-  modules: ['@nuxtjs/tailwindcss'],
-
-  // ❗️Change this:
-  ssr: true, // disable server-side rendering
-
-  // ✅ Add this:
-  nitro: {
-    preset: 'static'
+  css: [
+    '@/assets/styles.css'
+  ],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  ssr: true,
+  router: {
+    options: {
+      hashMode: false
+    }
   },
+
   app: {
     baseURL: '/MyPortfolio/',
   }
